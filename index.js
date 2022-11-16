f32d1b059636d7d5fb0342c149d1a9ce863072cf
Beau Lazear <beaulazear@gmail.com>
2022-11-15 21:26:18 -0500

    some updates, not working

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // use https://postimg.cc/gallery/bsv98kk for image sources
 
 document.addEventListener('DOMContentLoaded', () => {
-    function makeDogCard() {
-        
+    function makeDogCard(dog) {
+
+        let dogCard = document.createElement("div")
+        let dogImg = document.createElement("img")
+        let dogName = document.createElement("h2")
+        let dogBio = document.createElement("p")
+
+        dogImg.innerHTML = dog.image
+        dogName.innerText = dog.dogName
+        dogBio.innerText = dog.bio
+
+        document.getElementById("cardContainer").append(dogCard, dogImg, dogName, dogBio)
     }
+    fetch("db.json")
+    .then(resp => resp.json())
+    .then(dog => makeDogCard(dog))
+    
+    console.log("hello!")
 })
