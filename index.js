6ba2dafb59c7fdfdea956895a0e30c04e7f7abf6
Beau Lazear <beaulazear@gmail.com>
2022-11-17 23:41:04 -0500

    pulling everything together, almost there

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 // use https://postimg.cc/gallery/bsv98kk for image sources
 
 document.addEventListener('DOMContentLoaded', () => {
-    function makeDogCard(dog) {
 
-        let dogCard = document.createElement("div")
-        let dogImg = document.createElement("img")
-        let dogName = document.createElement("h2")
-        let dogBio = document.createElement("p")
+    let myForm = document.getElementById("search")
 
-        dogImg.innerHTML = dog.image
-        dogName.innerText = dog.dogName
-        dogBio.innerText = dog.bio
+    console.log(myForm)
 
-        document.getElementById("cardContainer").append(dogCard, dogImg, dogName, dogBio)
+    myForm.addEventListener("submit", (e) => {
+
+        e.preventDefault();
+
+        let dayOfWeek = document.getElementById("input").value
+
+        fetch("http://localhost:3000/dogs")
+            .then(resp => resp.json())
+            .then(dogs => sortDogs(dogs))
+
+        function sortDogs(dogs) {
+            dogs.forEach(dog => {
+                if (dog[dayOfWeek] === "yes") {
+                    renderDogCard(dog)
+                }
+        })
     }
-    fetch("/Users/beaulazear/Development/code/Phase-1/phase1project/db.json")
-    .then(resp => resp.json())
-    .then(dog => makeDogCard(dog))
+    })
 
-    console.log("hello!")
+    function renderDogCard(dog) {
+
+        let dogCard = document.createElement("ul")
+        dogCard.className = "dogCard"
+        dogCard.innerHTML = `
+        <img src="${dog.image}" width="300px" height="400"/>
+        <div class="dogContent">
+            <h3>${dog.name}</h3>
+            <p>${dog.bio}</p>
+            <p>${dog.name} gets walked for ${dog.walkDuration}!</p>
+            <p>Pick up ${dog.name} at ${dog.address} around ${dog.walkTime}</p>
+            </div>
+            <button>Cancel Walk</button><br><br>`
+
+        // let dogCard = document.createElement("div")
+        // let dogImg = document.createElement("img")
+        // let dogName = document.createElement("h2")
+        // let dogBio = document.createElement("p")
+
+        // dogImg.innerHTML = dog.image
+        // dogName.innerText = dog.name
+        // dogBio.innerText = dog.bio
+
+        document.getElementById("cardContainer").append(dogCard)
+
+        console.log(dog.image)
+    }
+
+
 })
