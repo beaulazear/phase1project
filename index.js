8f21fbb8bc9d2c01f47721633ba9ba06b6eadb3f
Beau <beaulazear@gmail.com>
2022-11-28 19:23:50 -0500

    cleand up code and got rid of page reloaad

--- index.js
+++ index.js
@@ -1,87 +1,87 @@
-// use https://postimg.cc/gallery/bsv98kk for image sources
-
 document.addEventListener('DOMContentLoaded', () => {
 
     let myForm = document.getElementById("day")
 
-    myForm.addEventListener("change", (e) => {
+    myForm.addEventListener("change", () => {
+        fetch("http://localhost:3000/dogs")
+        .then(resp => resp.json())
+        .then(dogs => {
+            sortDogs(dogs)
+            removeWalk(dogs)
+        })
+    })
 
-        e.preventDefault();
+    function removeWalk() {
+        
+        let cancelBtns = document.querySelectorAll('.cancelBtn')
+        
+        cancelBtns.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                e.target.parentElement.remove()
+            })
+        })
 
+        cancelBtns.forEach(btn => {
+            btn.addEventListener("mouseover", (e) => {
+                e.target.textContent = "Are you sure?"
+                e.target.style.backgroundColor = 'yellow'
+            })
+        })
+        
+        cancelBtns.forEach(btn => {
+            btn.addEventListener("mouseout", (e) => {
+                e.target.textContent = "Cancel walk"
+                e.target.style.backgroundColor = '#e9967a'
+            })
+        })
+    }
+    
+    function sortDogs(dogs) {
+        
+        removeChildNodes()
+        
+        let todaysWalksArr = []
         let dayOfWeek = document.getElementById("day").value
 
-        fetch("http://localhost:3000/dogs")
-            .then(resp => resp.json())
-            .then(dogs => sortDogs(dogs))
-            .then(dogs => removeWalk(dogs))
-
-            function sortDogs(dogs) {
-                removeChildNodes()
-
-                let todaysWalksArr = []
-
-                for(let i = 0; i < dogs.length; i++) {
-                    if (dogs[i][dayOfWeek] === "yes") {
-                        renderDogCard(dogs[i])
-                        todaysWalksArr.push(dogs[i])
-                    }
-                }
-                if (todaysWalksArr.length < 1) {
-                    removeChildNodes()
-                    renderNoWalkMessage()
-                }
+        for (let i = 0; i < dogs.length; i++) {
+            if (dogs[i][dayOfWeek] === "yes") {
+                renderDogCard(dogs[i])
+                todaysWalksArr.push(dogs[i])
             }
+        }
+        
+        if (todaysWalksArr.length < 1) {
+            removeChildNodes()
+            renderNoWalkMessage()
+        }
+    }
+    
+    function renderNoWalkMessage() {
 
-            function renderNoWalkMessage() {
-                removeChildNodes()
+        removeChildNodes()
 
-                let noWalkMessage = document.createElement("div")
-                noWalkMessage.id = "noWalkMessage"
-                noWalkMessage.innerHTML = `
-                <div>
-                    <p>There are no walks scheduled today, enjoy your day off!</p>
-                </div>`
-
-                document.getElementById("cardContainer").append(noWalkMessage)
-            }
-
-            function removeWalk() {
-
-                let cancelBtn = document.querySelectorAll('.cancelBtn')
-
-                cancelBtn.forEach(btn => {
-                    btn.addEventListener('click', (e) => {
-                        e.target.parentElement.remove()
-                    })
-                })
-                cancelBtn.forEach(btn => {
-                    btn.addEventListener("mouseover", (e) => {
-                        e.target.textContent = "Are you sure?"
-                        e.target.style.backgroundColor = 'yellow'
-                    })
-                })
-                cancelBtn.forEach(btn => {
-                    btn.addEventListener("mouseout", (e) => {
-                        e.target.textContent = "Cancel walk"
-                        e.target.style.backgroundColor = '#e9967a'
-                    })
-                })
-            }
-    })
+        let noWalkMessage = document.createElement("div")
+        noWalkMessage.id = "noWalkMessage"
+        noWalkMessage.innerHTML = `
+            <div>
+            <p>There are no walks scheduled today, enjoy your day off!</p>
+            </div>`
+        document.getElementById("cardContainer").append(noWalkMessage)
+    }
 
     function removeChildNodes() {
 
         let parent = document.getElementById("cardContainer")
 
-         while (parent.firstChild) {
+        while (parent.firstChild) {
             parent.removeChild(parent.firstChild)
-            }
         }
+    }
 
     function renderDogCard(dog) {
 
         let dogCard = document.createElement("ul")
-        
+
         dogCard.className = "dogCard"
         dogCard.innerHTML = `
         <div class="dogContent">
@@ -96,32 +96,32 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById("cardContainer").append(dogCard)
 
     }
+
     document.getElementById("newDogForm").addEventListener("submit", (e) => {
 
         e.preventDefault()
-        console.log(e.target.formDogName)
 
         let newDogObj = {
-            name:e.target.formDogName.value,
-            image:e.target.dogFormImg.value,
-            bio:e.target.dogBio.value,
-            address:e.target.dogFormAddress.value,
-            walkTime:e.target.dogFormWalkTime.value,
-            walkDuration:e.target.dogFormWalkDuration.value,
-            Unscheduled:'yes'
+            name: e.target.formDogName.value,
+            image: e.target.dogFormImg.value,
+            bio: e.target.dogBio.value,
+            address: e.target.dogFormAddress.value,
+            walkTime: e.target.dogFormWalkTime.value,
+            walkDuration: e.target.dogFormWalkDuration.value,
+            Unscheduled: 'yes'
         }
-
-        console.log(JSON.stringify(newDogObj))
 
         fetch('http://localhost:3000/dogs', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body:JSON.stringify(newDogObj)
+            body: JSON.stringify(newDogObj)
         })
-        .then(document.getElementById("newDogForm").reset())
-        .then(location.reload())
-
+            .then((resp) => resp.json())
+            .then((data) => {
+                document.getElementById("newDogForm").reset()
+                window.alert("Dog uploaded to database")
+            })
     })
 })
